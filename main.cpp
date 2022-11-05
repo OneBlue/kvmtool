72c2884aa2aab9a2ad0530e1ded909325570193a
Blue <blue@bluecode.fr>
2022-11-05 15:00:40 -0700

    Add logic to trigger window restore after the last screen resize event

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <thread>
 #include <sstream>
+#include <queue>
 #include <optional>
 #include <X11/extensions/randr.h>
 #include <X11/extensions/Xrandr.h>
@@ -48,8 +49,8 @@ void RestoreWindows(std::vector<WindowState>& state)
   {
     try
     {
-      std::cerr << "Restoring window: " << e.window.Title() << " -> "
-                << e.position << std::endl;
+      std::cerr << "Restoring window: " << e.window.WindowHandle() << " ("
+                << e.window.Title() << ") -> " << e.position << std::endl;
 
       e.window.SetPosition(e.position);
     }
@@ -61,10 +62,49 @@ void RestoreWindows(std::vector<WindowState>& state)
   }
 }
 
+void ConsumeEvents(Display* display,
+                   size_t timeout_ms,
+                   std::queue<XEvent>& events)
+{
+  auto deadline =
+      std::chrono::system_clock::now() + std::chrono::milliseconds(timeout_ms);
+
+  while (std::chrono::system_clock::now() < deadline)
+  {
+    while (XEventsQueued(display, QueuedAlready) > 0)
+    {
+      events.emplace();
+      XNextEvent(display, &events.back());
+      deadline = std::chrono::system_clock::now() +
+                 std::chrono::milliseconds(timeout_ms);
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  }
+}
+
+XEvent NextEvent(Display* display, std::queue<XEvent>& queue)
+{
+  if (queue.empty())
+  {
+    XEvent event{};
+    XNextEvent(display, &event);
+    return event;
+  }
+  else
+  {
+    XEvent event = queue.front();
+    queue.pop();
+
+    return event;
+  }
+}
+
 void Run(Display* display,
          XWindow root,
          size_t period_ms,
          size_t event_timeout_ms,
+         size_t resize_timeout_ms,
          int original_x,
          int original_y,
          const std::vector<std::string>& exclude,
@@ -87,9 +127,9 @@ void Run(Display* display,
   std::vector<WindowState> state;
   bool all_screens_present = true;
   timepoint last_event_ts;
+  std::queue<XEvent> queued_events;
   while (true)
   {
-
     /* Race condition: It's in theory possible that the resolution changed
      * just after the last XPending() call. That's why we have this extra
      * timeout check here*/
@@ -102,10 +142,9 @@ void Run(Display* display,
     }
 
     std::this_thread::sleep_for(std::chrono::milliseconds(period_ms));
-    while (XPending(display))
+    while (!queued_events.empty() || XPending(display))
     {
-      XEvent event;
-      XNextEvent(display, &event);
+      XEvent event = NextEvent(display, queued_events);
 
       last_event_ts = std::chrono::system_clock::now();
       if (event.type != rr_event_base + RRScreenChangeNotify)
@@ -122,6 +161,10 @@ void Run(Display* display,
       if (!all_screens_present && original_screens)
       {
         std::cerr << "Original screens detected" << std::endl;
+
+        // Wait a fixed amount of time to make sure all events are received
+        ConsumeEvents(display, resize_timeout_ms, queued_events);
+
         RestoreWindows(state);
       }
       else if (all_screens_present && !original_screens)
@@ -198,6 +241,7 @@ int main(int argc, char** argv)
                       {"screen-timeout", required_argument, 0, 's'},
                       {"exclude", required_argument, 0, 'e'},
                       {"foreground-when-lost", required_argument, 0, 'f'},
+                      {"resize-timeout", required_argument, 0, 'i'},
                       {"help", no_argument, 0, 'h'},
                       {0, 0, 0, 0}};
 
@@ -217,6 +261,7 @@ int main(int argc, char** argv)
   int x = -1;
   int y = -1;
   size_t refresh_timeout = 5000;
+  size_t resize_timeout = 2000;
   size_t screen_timeout = 2000;
   std::optional<std::string> foreground_when_lost;
 
@@ -253,6 +298,10 @@ int main(int argc, char** argv)
         foreground_when_lost = optarg;
         break;
 
+      case 'i':
+        resize_timeout = parse_int(optarg);
+        break;
+
       case 'e':
         std::istringstream str(optarg);
         std::string window;
@@ -285,6 +334,7 @@ int main(int argc, char** argv)
       XWindow{display, root},
       refresh_timeout,
       screen_timeout,
+      resize_timeout,
       x,
       y,
       exclude,
